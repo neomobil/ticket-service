6cd9f47003835fcaa0f55d5ac1309a9de1577054
Kornel Tako <takokornel@gmail.com>
2023-04-14 11:57:00 +0200

    Create quasar app including .env and firebase

---
+++ quasar-frontend/src/boot/firebase.ts
@@ -0,0 +1,20 @@
+import { boot } from 'quasar/wrappers'
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export default boot(({app}) => {
+
+app.config.globalProperties.$firebase = firebase;
+})
+
+export {firebase};
+
